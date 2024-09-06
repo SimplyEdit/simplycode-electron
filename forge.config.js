261042a9f82dc3a176303a3db1f65f4b2ddd75c1
govert@muze.nl <govert@muze.nl>
2024-09-06 10:14:29 +0200

    added required 'path' constant

--- forge.config.js
+++ forge.config.js
@@ -1,5 +1,6 @@
 const { FusesPlugin } = require('@electron-forge/plugin-fuses');
 const { FuseV1Options, FuseVersion } = require('@electron/fuses');
+const path = require('path');
 
 module.exports = {
   packagerConfig: {
