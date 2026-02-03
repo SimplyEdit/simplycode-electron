671192db920034b037692f4a92c6194822bbf363
Yvo Brevoort <yvo@muze.nl>
2026-02-03 17:22:08 +0100

    update to 3.5.3

--- simplycode/js/simply.everything.js
+++ simplycode/js/simply.everything.js
@@ -164,8 +164,6 @@
       if (!path2) {
         if (this.has(document.location.pathname + document.location.hash)) {
           path2 = document.location.pathname + document.location.hash;
-        } else if (this.has(document.location.hash)) {
-          path2 = document.location.hash;
         } else {
           path2 = document.location.pathname;
         }
@@ -298,7 +296,7 @@
     if (path2.substring(0, baseURL.length) == baseURL || baseURL[baseURL.length - 1] == "/" && path2.length == baseURL.length - 1 && path2 == baseURL.substring(0, path2.length)) {
       path2 = path2.substring(baseURL.length);
     }
-    if (path2[0] != "/" && path2[0] != "#") {
+    if (path2[0] != "/") {
       path2 = "/" + path2;
     }
     return path2;
@@ -314,10 +312,13 @@
     return baseURL + path2;
   }
   function getRegexpFromRoute(route, exact = false) {
-    if (exact) {
-      return new RegExp("^" + route.replace(/:\w+/g, "([^/]+)").replace(/:\*/, "(.*)") + "(\\?|$)");
+    if (route[0] != "#") {
+      route = "^" + route;
     }
-    return new RegExp("^" + route.replace(/:\w+/g, "([^/]+)").replace(/:\*/, "(.*)"));
+    if (exact) {
+      return new RegExp(route.replace(/:\w+/g, "([^/]+)").replace(/:\*/, "(.*)") + "(\\?|$)");
+    }
+    return new RegExp(route.replace(/:\w+/g, "([^/]+)").replace(/:\*/, "(.*)"));
   }
   function parseRoutes(routes2, routeInfo, exact = false) {
     const paths = Object.keys(routes2);
@@ -537,38 +538,35 @@
           keyboardElement = keyboardElement.parentNode.closest("[data-simply-keyboard]");
         }
         keyboards.push("");
-        let keyboard, subkeyboard;
+        let keyboard;
         let separators = ["+", "-"];
         for (let i in keyboards) {
           keyboard = keyboards[i];
           if (keyboard == "") {
-            subkeyboard = "default";
-          } else {
-            subkeyboard = keyboard;
-            keyboard += ".";
+            keyboard = "default";
           }
           for (let separator of separators) {
             let keyString = keyCombination.join(separator);
-            if (this[subkeyboard] && typeof this[subkeyboard][keyString] == "function") {
-              let _continue = this[subkeyboard][keyString].call(options.app, e);
+            if (this[keyboard] && typeof this[keyboard][keyString] == "function") {
+              let _continue = this[keyboard][keyString].call(options.app, e);
               if (!_continue) {
                 e.preventDefault();
                 return;
               }
             }
-            if (typeof this[subkeyboard + keyString] == "function") {
-              let _continue = this[subkeyboard + keyString].call(options.app, e);
+            if (typeof this[keyboard + "." + keyString] == "function") {
+              let _continue = this[keyboard + "." + keyString].call(options.app, e);
               if (!_continue) {
                 e.preventDefault();
                 return;
               }
             }
-            if (this[selectedKeyboard] && this[selectedKeyboard][keyString]) {
-              let targets = options.app.container.querySelectorAll('[data-simply-accesskey="' + keyboard + keyString + '"]');
-              if (targets.length) {
-                targets.forEach((t) => t.click());
-                e.preventDefault();
-              }
+            const qsa = '[data-simply-accesskey="' + keyboard + "." + keyString + '"]';
+            const targets = options.app.container.querySelectorAll(qsa);
+            console.log(qsa, targets);
+            if (targets.length) {
+              targets.forEach((t) => t.click());
+              e.preventDefault();
             }
           }
         }
